518367a3d3987b63828c66fbdf6c75972d5ea2ae
bar0026 <asilbek.boltayev0626@gmail.com>
2026-01-30 11:09:38 +0500

    8-sinf-bot

--- 8-sinf-bot.py
+++ 8-sinf-bot.py
@@ -8,7 +8,7 @@ from telebot import types
 from pymongo import MongoClient
 
 # ======================
-# LOGGING
+# LOGING
 # ======================
 logging.basicConfig(level=logging.INFO)
 
@@ -253,3 +253,4 @@ if __name__ == "__main__":
     set_webhook()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
